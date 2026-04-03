cd39326f50f9110cff55481415c731c525644dd0
Melisande Onana <melisande.onana@orange.fr>
2026-04-03 11:26:21 +0200

    Added endpoints to retrieve the number of pending laundries and the list of pending laundries

--- src/Repository/LaundryRepository.php
+++ src/Repository/LaundryRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Laundry;
+use App\Enum\LaundryStatusEnum;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +15,19 @@ class LaundryRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Laundry::class);
+    }
+
+    /**
+     * Count pending laundries that are not soft-deleted.
+     */
+    public function countPendingLaundries(): int
+    {
+        return (int) $this->createQueryBuilder('l')
+            ->select('COUNT(l.id)')
+            ->where('l.status = :status')
+            ->andWhere('l.deletedAt IS NULL')
+            ->setParameter('status', LaundryStatusEnum::PENDING)
+            ->getQuery()
+            ->getSingleScalarResult();
     }
 }
