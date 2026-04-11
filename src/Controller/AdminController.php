ec8ec88fd2d41ae014b61f1e53b38c3e9b263224
MWmdauguet <m.dauguet@mentalworks.fr>
2026-04-11 21:36:28 +0200

    add api routes for professional count and laundries pending

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,12 +14,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdminController extends AbstractController
 {
-
     #[Route('/api/admin/profile', name: 'api_admin_profile_get', methods: ['GET'])]
     public function getProfile(): JsonResponse
     {
@@ -32,6 +32,30 @@ class AdminController extends AbstractController
         return $this->json([
             'id' => $user->getId(),
             'email' => $user->getEmail(),
+        ]);
+    }
+
+    #[Route('/api/admin/professionals/pending/count', name: 'api_admin_professionals_pending_count', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function getPendingProfessionalsCount(
+        ProfessionalRepository $professionalRepository
+    ): JsonResponse
+    {
+        $user = $this->getUser();
+        
+        if (!$user instanceof Admin) {
+            return $this->json(['error' => 'errors.unauthorized'], 403);
+        }
+
+        try {
+            $total = $professionalRepository->countPendingProfessionals();
+        } catch (\Exception $e) {
+            return $this->json(['error' => 'errors.fetch_error'], 500);
+        }
+
+        return $this->json([
+            'total' => $total,
+            'response' => Response::HTTP_OK,
         ]);
     }
 
