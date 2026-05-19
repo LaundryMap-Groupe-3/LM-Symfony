dd59da1e253acbf4d06ed4510990c62e676ca3e9
Allan Decaux <a.decaux@mentalworks.fr>
2026-05-19 15:13:02 +0200

    big fixes part 5

--- src/DataFixtures/PaymentMethodFixtures.php
+++ src/DataFixtures/PaymentMethodFixtures.php
@@ -9,8 +9,10 @@ use Doctrine\Persistence\ObjectManager;
 class PaymentMethodFixtures extends Fixture
 {
     public const CARD_NAME = 'Card';
-    public const CASH_NAME = 'Cash';
     public const CONTACTLESS_NAME = 'Contactless';
+    public const COINS_NAME = 'Coins';
+    public const BILLS_NAME = 'Bills';
+    public const FIDELITY_NAME = 'Fidelity';
 
     public function load(ObjectManager $manager): void
     {
@@ -18,13 +20,21 @@ class PaymentMethodFixtures extends Fixture
         $card->setName(self::CARD_NAME);
         $manager->persist($card);
 
-        $cash = new PaymentMethod();
-        $cash->setName(self::CASH_NAME);
-        $manager->persist($cash);
-
         $contactless = new PaymentMethod();
         $contactless->setName(self::CONTACTLESS_NAME);
         $manager->persist($contactless);
+
+        $coins = new PaymentMethod();
+        $coins->setName(self::COINS_NAME);
+        $manager->persist($coins);
+
+        $bills = new PaymentMethod();
+        $bills->setName(self::BILLS_NAME);
+        $manager->persist($bills);
+
+        $fidelity = new PaymentMethod();
+        $fidelity->setName(self::FIDELITY_NAME);
+        $manager->persist($fidelity);
 
         $manager->flush();
     }
